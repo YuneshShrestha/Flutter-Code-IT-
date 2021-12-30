f2333c5ee127b52c6b3a75edd5580a30c375b369
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-12-30 21:05:14 +0545

    saying api

--- lib/view/screen/about.dart
+++ lib/view/screen/about.dart
@@ -1,13 +1,60 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class About extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class About extends StatefulWidget {
   const About({Key? key}) : super(key: key);
+
+  @override
+  State<About> createState() => _AboutState();
+}
+
+class _AboutState extends State<About> {
+  Map data = {};
+  bool? isLoading;
+  Future getSaying() async {
+    var response =
+        await http.get(Uri.parse('https://api.adviceslip.com/advice'));
+    setState(() {
+      data = jsonDecode(response.body);
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getSaying();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            isLoading = true;
+          });
+          getSaying();
+        },
+        child: const Icon(Icons.sync),
+      ),
       appBar: AppBar(
-        title: const Text("About Page"),
+        title: const Text("Day By Day Saying"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Saying Of The Day",
+                style: TextStyle(color: Colors.grey, fontSize: 20)),
+            isLoading != true
+                ? Text(data['slip']['advice'])
+                : const CircularProgressIndicator()
+          ],
+        ),
       ),
     );
   }
